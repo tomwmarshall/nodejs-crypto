41cd643738d95ab52449364069ccfa7e53f88d69
Tom Marshall <zbva946@live.rhul.ac.uk>
2016-04-18 10:45:59 +0100

    Initial crypto functionality

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// https://github.com/tomwmarshall/nodejs-crypto.git
+
+var crypto = require('crypto'),
+    algorithm = 'aes-256-ctr',
+    password = 'd6F3Efeq';
+
+function encrypt(text){
+    var cipher = crypto.createCipher(algorithm,password)
+    var crypted = cipher.update(text,'utf8','hex')
+    crypted += cipher.final('hex');
+    return crypted;
+}
+
+function decrypt(text){
+    var decipher = crypto.createDecipher(algorithm,password)
+    var dec = decipher.update(text,'hex','utf8')
+    dec += decipher.final('utf8');
+    return dec;
+}
+
+var m = encrypt("Enki encryption.")
+console.log(m);
+console.log(decrypt(m));
